c8a1155700d5a7e0af5757615925e79d895ca572
Alexander Dalbert <dalbert.alexander@gmail.com>
2021-02-05 13:41:41 +0100

    HelloWorld2

--- A2B-Zoom.ino
+++ A2B-Zoom.ino
@@ -10,47 +10,30 @@
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
-/*----------------------------------*/ 	      
+
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 	      
 
 /* PinOut
  *  
  *  D1 = SCL
  *  D2 = SDA
 */////////////////////////////////////////////////////////////////
-#define ROTARY_PIN1  2 // Wemos-D1-mini D4
-#define ROTARY_PIN2 0 // Wemos-D1-mini D3
-#define BUTTON_PIN  3 // Wemos-D1-mini RX
 
-//int StepEnable = 16; // Enable Wemos-D1-mini D4
-int StepStep = 14; // Step Wemos-D1-mini D3
-int StepDir = 12 ; // Richtung Wemos-D1-mini D2
-
-
-
-/*----------------------------------*/
+#define ROTARY_PIN1  2 // D4
+#define ROTARY_PIN2 0 // D3
+#define BUTTON_PIN  3 // RX
 
 /* Drehregler */
 /////////////////////////////////////////////////////////////////
 #define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
 ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
 Button2 b = Button2(BUTTON_PIN);
-/*----------------------------------*/
 
 
 /* FollowZoom */
 /////////////////////////////////////////////////////////////////
 int Range = 0;
 int Position = 0;
-/*----------------------------------*/
-
-/* Menue */
-/////////////////////////////////////////////////////////////////
-bool SetMenu = true;
-int SetMenuBlink = 0;
-int BlinkTime = 20000;
-int BlinkTimeCount = 0;
-/*----------------------------------*/
 
 
 
@@ -59,12 +42,12 @@ void setup(void)
   Serial.begin(9600);
   delay(50);
   Serial.println("\n\nSlider Setup");
-  /*----------------------------------*/
+  
 
   // Setup Button Rotate
-  r.setChangedHandler(HumanDetector);
-  r.setLeftRotationHandler(minus);
-  r.setRightRotationHandler(plus);
+  r.setChangedHandler(rotate);
+  r.setLeftRotationHandler(showDirection);
+  r.setRightRotationHandler(showDirection);
   b.setTapHandler(click);
   b.setLongClickHandler(resetPosition);
   /*----------------------------------*/
@@ -72,126 +55,48 @@ void setup(void)
   // Setup Display
   u8x8.begin();
   u8x8.setPowerSave(0);
-  /*----------------------------------*/
-
-
-  // Setup Motor
-  //pinMode(StepEnable, OUTPUT); // Enable
-  pinMode(StepStep, OUTPUT); // Step
-  pinMode(StepDir, OUTPUT); // Richtung
-  /*----------------------------------*/
+  
+  
 }
 
 void loop(void)
 {
-  if(SetMenu){menue();}
-  spin();
 
-}
+  // Große Schrift
+  //u8x8.setFont(u8x8_font_px437wyse700a_2x2_r);
+  //u8x8.drawString(0, 0, "ABC defg");
 
-/* Funktion Drehregler */
-/////////////////////////////////////////////////////////////////
-
-// on change
-void HumanDetector(ESPRotary& r) {
-  //oled(r.getPosition());
-  SetMenu = false;
+  //u8x8.setFont(u8x8_font_px437wyse700b_2x2_r);
+  //u8x8.drawString(0, 2, "ABC defg");
   
-}
-
-// on left or right rotation
-void showDirection(ESPRotary& r) {
-  Serial.println(r.directionToString(r.getDirection()));
-}
-
-void plus(ESPRotary& r){
-  stepMotorPlus();
-  Serial.println("stepPlus");
-  }
-void minus(ESPRotary& r){
-  stepMotorMinus();
-  Serial.println("stepMinus");
-  }
- 
-// single click
-void click(Button2& btn) {
-  Serial.println("Click!");
-}
-
-// long click
-void resetPosition(Button2& btn) {
-  // r.resetPosition();
-  Serial.println("Langer Klick");
-}
-/*----------------------------------*/
-
-void oled(int p){
-  u8x8.clear();
+  // Kleine Schrift
   u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.drawString(0,1,"<-Start  Ende->");
   u8x8.setInverseFont(1);
-  u8x8.drawString(0,2,String(p).c_str());
-  u8x8.refreshDisplay();
-}
-
-void menue(){
-  // Überschrift
-  u8x8.clear();
-  u8x8.setFont(u8x8_font_px437wyse700b_2x2_r);
-  u8x8.drawString(0, 0, "A2B-Zoom");
-  /*----------------------------------*/
-    
-  // Fusszeile
+  u8x8.drawString(0,2,String(Test).c_str());
+  u8x8.drawString(0,3,"******** 89%");
+  u8x8.refreshDisplay();		// only required for SSD1606/7  
+  delay(2000);
+  
+  /* Auf den Kopf
+   *  
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-      u8x8.setInverseFont(SetMenuBlink);
-      u8x8.drawString(0,7,"made by Dalbyte");
-  /*----------------------------------*/
-      
-  while (SetMenu) {
-      ++BlinkTimeCount;
-      if(BlinkTimeCount >= BlinkTime){
-      if(SetMenuBlink==0){
-        SetMenuBlink=1;
-        BlinkTimeCount=0;
-      }else{
-        SetMenuBlink=0;
-        BlinkTimeCount=0;
-      }
-      PleaseInput();
-      }
-      spin();
-  }
-}
-void PleaseInput(){
- u8x8.setFont(u8x8_font_chroma48medium8_r);
- u8x8.setInverseFont(SetMenuBlink);
- u8x8.drawString(0,4,"   Dreh mich!   ");
- u8x8.refreshDisplay();
-}
+  
+  u8x8.clear();
+  u8x8.setFlipMode(0);
+  u8x8.setInverseFont(1);
+  u8x8.drawString(0,0,"FlipMode 0");
+  u8x8.setInverseFont(0);
+  u8x8.drawString(1,1,"FlipMode 0");
+  delay(1500);
 
-void spin(){
-  r.loop();
-  b.loop();
-}
-
-
-/* DrehenDirekt */
-/////////////////////////////////////////////////////////////////
-void stepMotorPlus(){
-
-  digitalWrite(StepDir,HIGH); // im Uhrzeigersinn
-  digitalWrite(StepStep,HIGH);
-  delayMicroseconds(500);
-  digitalWrite(StepStep,LOW);
-  delayMicroseconds(500);
-
-}
-
-void stepMotorMinus(){
-
-  digitalWrite(StepDir,LOW); // im Uhrzeigersinn
-  digitalWrite(StepStep,HIGH);
-  delayMicroseconds(500);
-  digitalWrite(StepStep,LOW);
-  delayMicroseconds(500);
-
+  u8x8.clear();
+  u8x8.setFlipMode(1);
+  u8x8.setInverseFont(1);
+  u8x8.drawString(0,0,"FlipMode 1");
+  u8x8.setInverseFont(0);
+  u8x8.drawString(1,1,"FlipMode 1");
+  delay(1500);
+  */
+  
 }
